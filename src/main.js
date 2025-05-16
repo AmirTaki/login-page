46b450b2afc0da9263d9be6777cfe7db8576b79e
amir <amirtaki623@gmail.com>
2025-05-16 14:35:25 +0330

    edit styles keyfream & dom js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
-form = document.querySelectorAll('form')
+form = document.querySelectorAll('.options-02 a')
 
-
-
-document.querySelector('#one').addEventListener('click',()=>{
-   document.querySelector('#form-one').className = 'animatinformOne'
+form.forEach(element => {
+   element.addEventListener("click", (event)=>{
+      if(element.innerText == "Create an Account"){
+            
+         document.querySelector('#form-one').className = 'animatinformOne'
  
+         document.querySelector('#form-two').className = 'animatinformtTwo'
+      }
+      else {
+         document.querySelector('#form-two').className = 'animatinformtTwoR'
+         
+         document.querySelector('#form-one').className = 'animatinformOneR'
+      }
 
-   document.querySelector('#form-two').className = 'animatinformtTwo'
-
-
-})
-
-
-document.querySelector('#two').addEventListener('click',()=>{
-   
- 
-
-   document.querySelector('#form-two').className = 'animatinformtTwoR'
-     document.querySelector('#form-one').className = 'animatinformOneR'
+   })
+});
 
 
-})
+window.onload = () =>{
+      
+      document.querySelector('#form-one').className = 'animatinformOneR'
 
+}
+      
